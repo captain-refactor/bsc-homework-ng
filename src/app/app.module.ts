42054f6c171a62a3bde9946f33a3533aaf3318b3
Jan Kremen <kremenh@gmail.com>
2020-06-03 00:38:50 +0200

    refine app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {RoutingModule} from "./routing/routing.module";
-import {MatSelectModule} from "@angular/material/select";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {AppRoutingModule} from './app-routing/app-routing.module';
+import {MatSelectModule} from '@angular/material/select';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatListModule} from '@angular/material/list';
+import {HttpClientModule} from '@angular/common/http';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -14,10 +20,17 @@ import {MatSelectModule} from "@angular/material/select";
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    RoutingModule,
-    MatSelectModule
+    AppRoutingModule,
+    MatSelectModule,
+    MatGridListModule,
+    MatListModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
