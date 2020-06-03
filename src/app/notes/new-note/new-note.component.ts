b54b4c24408f1bc57781128661741a6e19582cd6
Jan Kremen <kremenh@gmail.com>
2020-06-03 15:18:32 +0200

    finalize new note component

--- src/app/notes/new-note/new-note.component.ts
+++ src/app/notes/new-note/new-note.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiNewNote} from '../notes-client.service';
+import {NotesService} from '../notes.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-new-note',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewNoteComponent implements OnInit {
 
-  constructor() { }
+  note: ApiNewNote = {title: ''};
+
+  constructor(private service: NotesService, private router: Router, private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
+    this.note = {title: ''};
+  }
+
+  async createNote() {
+    const note = await this.service.createNote(this.note).toPromise();
+    await this.router.navigate(['..', note.id], {relativeTo: this.route});
   }
 
 }
