143d663bd5e7dfcbb5a2465538dc811075194ba8
Jan Kremen <kremenh@gmail.com>
2020-06-03 15:19:04 +0200

    add necessary imports

--- src/app/notes/notes.module.ts
+++ src/app/notes/notes.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NotesComponent} from './notes/notes.component';
-import {NotesListComponent} from './notes-list/notes-list.component';
 import {NotesRoutingModule} from './notes-routing/notes-routing.module';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
@@ -9,16 +8,20 @@ import {FlexModule} from '@angular/flex-layout';
 import {MatButtonModule} from '@angular/material/button';
 import { NoteDetailComponent } from './note-detail/note-detail.component';
 import { NewNoteComponent } from './new-note/new-note.component';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [NotesComponent, NotesListComponent, NoteDetailComponent, NewNoteComponent],
+  declarations: [NotesComponent, NoteDetailComponent, NewNoteComponent],
   imports: [
     CommonModule,
     NotesRoutingModule,
     MatIconModule,
     MatListModule,
     FlexModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    FormsModule
   ]
 })
 export class NotesModule {
