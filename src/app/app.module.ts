e5d188582205c0ed945c4e018595eb4831e070f8
Jan Kremen <kremenh@gmail.com>
2020-06-03 15:20:22 +0200

    use responsive styling remove redundant code use on push change detection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,7 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {AppRoutingModule} from './app-routing/app-routing.module';
-import {MatSelectModule} from '@angular/material/select';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatListModule} from '@angular/material/list';
 import {HttpClientModule} from '@angular/common/http';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -21,13 +15,7 @@ import {MatIconModule} from '@angular/material/icon';
     BrowserAnimationsModule,
     MatToolbarModule,
     AppRoutingModule,
-    MatSelectModule,
-    MatGridListModule,
-    MatListModule,
     HttpClientModule,
-    FlexLayoutModule,
-    MatButtonModule,
-    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
