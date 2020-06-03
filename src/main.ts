8ed3874030159c2b50b0908808abc422139b67cd
Jan Kremen <kremenh@gmail.com>
2020-06-03 19:25:54 +0200

    add locales

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
+import {registerLocaleData} from '@angular/common';
+import localeCs from '@angular/common/locales/cs';
+import localeEn from '@angular/common/locales/en';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+registerLocaleData(localeCs);
+registerLocaleData(localeEn, 'en-US');
 
 if (environment.production) {
   enableProdMode();
