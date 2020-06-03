af67a8fc64460be09469e558315adf7e6c44f422
Jan Kremen <kremenh@gmail.com>
2020-06-03 15:18:21 +0200

    finalize notes detail component

--- src/app/notes/note-detail/note-detail.component.ts
+++ src/app/notes/note-detail/note-detail.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {NotesService} from '../notes.service';
+import {ActivatedRoute} from '@angular/router';
+import {switchMap} from 'rxjs/operators';
+import {ApiNote} from '../notes-client.service';
 
 @Component({
   selector: 'app-note-detail',
   templateUrl: './note-detail.component.html',
-  styleUrls: ['./note-detail.component.scss']
+  styleUrls: ['./note-detail.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NoteDetailComponent implements OnInit {
 
-  constructor() { }
+
+  note$ = this.route.params.pipe(switchMap(params => this.notesService.getNote(+params.id)));
+
+  constructor(private notesService: NotesService, private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
+  }
+
+  async updateNote(note: ApiNote) {
+    await this.notesService.updateNote(note).toPromise();
   }
 
 }
